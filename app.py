66adbf245e1cfc0700caaf0f383ece2d9153733c
haseeb2408 <112976050+haseeb2408@users.noreply.github.com>
2025-01-20 17:20:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
+from flask import Flask , render_template , request , jsonify
 import os
 import google.generativeai as genai
+from  geminiy_api import api_key
 
-genai.configure(api_key="AIzaSyDIly_Uvnhh1i5acecax8gwfRoZUYx1YSY")
-
-# Create the model
+genai.configure(api_key = api_key)
 generation_config = {
   "temperature": 1,
   "top_p": 0.95,
@@ -21,10 +21,40 @@ chat_session = model.start_chat(
   history=[
   ]
 )
-while True:
-    msg=input("Your Message : ")
-    response = chat_session.send_message(msg)
-    print(f"Gemini's response : {response.text}")
-    if msg == 'exit':
-        break
+
+
+app = Flask(__name__)
+
+
+@app.route("/chat" , methods = ['POST' , 'GET'])
+def chat():
+    response_text = None
+    if request.method == "POST":
+        try:
+            message = request.form.get('message')
+            response = chat_session.send_message(message)
+            response_text = response.text
+        except:
+            return "Error....."
+    return render_template('chat.html' , response = response_text)
         
+
+
+
+@app.route("/chatwithAI" , methods =['POST'])
+def chatwithAI():
+    try:
+        data = request.get_json()
+        try:
+            msg = data.get('message')
+            response = chat_session.send_message(msg)
+            response_msg = response.text
+            return jsonify({'response' : response_msg})
+        except:
+            return jsonify({"Error" : "'message' not found"})
+    except:
+        return jsonify({"Error" : "Some error in json object"})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
